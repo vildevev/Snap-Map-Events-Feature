318fc1045eeb3ebac43f05d8c81904189988ff75
“Vilde <vilde@vevatne.no>
2017-08-06 11:28:29 -0700

    Add longpressGestureRecognizer

--- Snap Map Events Feature/ViewController.swift
+++ Snap Map Events Feature/ViewController.swift
@@ -9,25 +9,35 @@
 import UIKit
 import MapKit
 
+var events = [Dictionary<String, String>()]
+
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     var locationManager = CLLocationManager()
     @IBOutlet var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-       locationManager.delegate = self
+        locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:))) 
+        uilpgr.minimumPressDuration = 2
+        map.addGestureRecognizer(uilpgr)
     }
     
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        
+        print("long press")
+    }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = manager.location?.coordinate {
             let center = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
             let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
             self.map.setRegion(region, animated: true)
-            self.map.removeAnnotations(self.map.annotations) 
+            self.map.removeAnnotations(self.map.annotations)
+            
             let annotation = MKPointAnnotation()
             annotation.coordinate = center
             annotation.title = "Your Location"
